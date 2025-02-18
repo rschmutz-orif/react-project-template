93535513215db53cc2cf70f41e3e2e99541458d3
rschmutz.pomy <rschmutz.pomy@gmail.com>
2025-02-18 10:35:54 +0000

    Update package

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'
-
+import { Counter } from '@rsch-org/react-npm-package'
 // Styles
 import './index.pcss';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<h1>Hello world</h1>);
+root.render(<>
+  <h1>Hello world</h1>
+  <Counter />
+</>);
